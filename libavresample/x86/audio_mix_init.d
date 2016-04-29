9f6bab06ba8ff4826a762be1cfe97558a8d1cf8b
Your Name <you@example.com>
2016-04-29 18:25:04 +0800

    Release 20160429

---
+++ libavresample/x86/audio_mix_init.d
@@ -0,0 +1,16 @@
+libavresample/x86/audio_mix_init.o: libavresample/x86/audio_mix_init.c \
+ config.h libavutil/cpu.h libavutil/attributes.h libavutil/x86/cpu.h \
+ libavutil/cpu_internal.h libavutil/cpu.h libavresample/audio_mix.h \
+ libavutil/samplefmt.h libavutil/avutil.h libavutil/common.h \
+ libavutil/macros.h libavutil/version.h libavutil/avconfig.h \
+ libavutil/intmath.h libavutil/x86/intmath.h libavutil/mem.h \
+ libavutil/error.h libavutil/internal.h libavutil/timer.h libavutil/log.h \
+ libavutil/x86/timer.h libavutil/dict.h libavutil/pixfmt.h \
+ libavutil/x86/emms.h libavutil/attributes.h libavutil/libm.h \
+ libavutil/intfloat.h libavutil/mathematics.h libavutil/rational.h \
+ libavresample/avresample.h libavutil/avutil.h libavutil/channel_layout.h \
+ libavutil/dict.h libavutil/frame.h libavutil/buffer.h \
+ libavutil/samplefmt.h libavutil/log.h libavutil/mathematics.h \
+ libavresample/version.h libavutil/version.h libavresample/internal.h \
+ libavutil/audio_fifo.h libavutil/fifo.h libavutil/opt.h \
+ libavresample/audio_data.h
---
+++ libavresample/x86/dither_init.d
@@ -0,0 +1,16 @@
+libavresample/x86/dither_init.o: libavresample/x86/dither_init.c config.h \
+ libavutil/cpu.h libavutil/attributes.h libavutil/x86/cpu.h \
+ libavutil/cpu_internal.h libavutil/cpu.h libavresample/dither.h \
+ libavresample/avresample.h libavutil/avutil.h libavutil/common.h \
+ libavutil/macros.h libavutil/version.h libavutil/avconfig.h \
+ libavutil/intmath.h libavutil/x86/intmath.h libavutil/mem.h \
+ libavutil/error.h libavutil/avutil.h libavutil/internal.h \
+ libavutil/timer.h libavutil/log.h libavutil/x86/timer.h libavutil/dict.h \
+ libavutil/pixfmt.h libavutil/x86/emms.h libavutil/attributes.h \
+ libavutil/libm.h libavutil/intfloat.h libavutil/mathematics.h \
+ libavutil/rational.h libavutil/channel_layout.h libavutil/dict.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/samplefmt.h \
+ libavutil/log.h libavutil/mathematics.h libavresample/version.h \
+ libavutil/version.h libavresample/audio_data.h libavutil/audio_fifo.h \
+ libavutil/fifo.h libavutil/samplefmt.h libavresample/internal.h \
+ libavutil/opt.h
