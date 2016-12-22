761963e39c1be25c0181963369c255a894cacc41
Magic Len <len@magiclen.org>
2016-12-22 15:52:34 +0800

    Release 20161222

---
+++ libavresample/arm/audio_convert_init.d
@@ -0,0 +1,17 @@
+libavresample/arm/audio_convert_init.o: \
+ libavresample/arm/audio_convert_init.c config.h libavutil/attributes.h \
+ libavutil/cpu.h libavutil/attributes.h libavutil/arm/cpu.h \
+ libavutil/cpu_internal.h libavutil/cpu.h libavutil/samplefmt.h \
+ libavutil/avutil.h libavutil/common.h libavutil/macros.h \
+ libavutil/version.h libavutil/avconfig.h libavutil/intmath.h \
+ libavutil/arm/intmath.h libavutil/mem.h libavutil/error.h \
+ libavutil/internal.h libavutil/timer.h libavutil/log.h \
+ libavutil/arm/timer.h libavutil/dict.h libavutil/pixfmt.h \
+ libavutil/libm.h libavutil/intfloat.h libavutil/mathematics.h \
+ libavutil/rational.h libavresample/audio_convert.h \
+ libavresample/avresample.h libavutil/avutil.h libavutil/channel_layout.h \
+ libavutil/dict.h libavutil/frame.h libavutil/buffer.h \
+ libavutil/samplefmt.h libavutil/log.h libavutil/mathematics.h \
+ libavresample/version.h libavutil/version.h libavresample/internal.h \
+ libavutil/audio_fifo.h libavutil/fifo.h libavutil/opt.h \
+ libavresample/audio_data.h
